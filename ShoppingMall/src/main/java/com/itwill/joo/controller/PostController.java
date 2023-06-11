9777a4eb0fb53e9e1e4f92a8aa3b7cc628923ce4
WonjunS <seowj0710@gmail.com>
2023-06-12 00:06:32 +0900

    [서원준] Feat: temporary01 브랜치에서 가져옴

---
+++ ShoppingMall/src/main/java/com/itwill/joo/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.itwill.joo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor // 생성자 자동 생성
+@RequestMapping("/post") // "/post" 로 시작하는 주소 다처리
+@Controller
+public class PostController {
+
+	@GetMapping("/postDetail")
+	public void postDetail() {
+	}
+
+	@GetMapping("/postList")
+	public void postList() {
+	}
+	
+}
---
+++ ShoppingMall/src/main/java/com/itwill/joo/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.itwill.joo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor // 생성자 자동 생성
+@RequestMapping("/product") // "/post" 로 시작하는 주소 다처리
+@Controller
+public class ProductController {
+
+	@GetMapping("/productsList")
+	public void productList() {
+
+	}
+
+	@GetMapping("/productDetail")
+	public void productsDetail() {
+
+	}
+
+	@GetMapping("/productsHotList")
+	public void productsHotList() {
+
+	}
+
+	@GetMapping("/productsNewList")
+	public void productsNewList() {
+
+	}
+
+}
