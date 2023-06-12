fec1729a1cf88fa419bf9636af39e1a5b701978d
WonjunS <seowj0710@gmail.com>
2023-06-12 10:57:52 +0900

    [서원준] Feat: 주문 관련 domain 병합

--- ShoppingMall/src/main/java/com/itwill/joo/domain/OrderProduct.java
+++ ShoppingMall/src/main/java/com/itwill/joo/domain/OrderProduct.java
@@ -12,10 +12,10 @@ import lombok.NoArgsConstructor;
 public class OrderProduct {
 	
 	private long id;
-	private long productId; // 주문한 상품 아이디
-	private long orderId; // 주문 아이디
-	
-	private int count; // 상품 수량
-	private int price; // 상품 가격
+	private long p_id; // 주문한 상품 아이디
+	private long o_id; // 주문 아이디
+
+	private int pcount; // 상품 수량
+	private int pprice; // 상품 가격
 
 }
