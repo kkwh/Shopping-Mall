a31d17a1f08a86901b9bac04ed79a6862beb27aa
WonjunS <seowj0710@gmail.com>
2023-06-15 15:15:33 +0900

    [서원준] Feat: 아이디, 비밀번호 찾기, 회원 상세 정보 페이지

--- ShoppingMall/src/main/java/com/itwill/joo/controller/UserController.java
+++ ShoppingMall/src/main/java/com/itwill/joo/controller/UserController.java
@@ -1,15 +1,23 @@
 package com.itwill.joo.controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.itwill.joo.dto.user.TestDto;
 import com.itwill.joo.dto.user.UserCreateDto;
+import com.itwill.joo.dto.user.UserDetailDto;
+import com.itwill.joo.dto.user.UserUpdateDto;
+import com.itwill.joo.service.MailService;
 import com.itwill.joo.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -69,14 +77,67 @@ public class UserController {
 	
 	@GetMapping("/findPassword")
 	public String finaPassword() {
-		return "";
+		return "user/findPassword";
 	}
 	
-	@GetMapping("/myQueries")
-	public String myQueries() {
-		log.info("myQueries()");
+	@PostMapping("/findPassword")
+	@ResponseBody
+	public int sendPassword(@RequestParam("name") String name, @RequestParam("email") String email) {
+		log.info("findPassword({}, {})", name, email);
 		
-		return "user/myQueries";
+		String loginId = userService.getLoginId(name, email);
+		if(loginId == null || loginId.equals("0")) {
+			return 0;
+		}
+		
+		String code = MailService.generateTemporaryPassword();
+		MailService.sendTemporaryPassword(email, code);
+		log.info("code = {}", code);
+		
+		log.info("updatePassword({}, {})", loginId, code);
+		int result = userService.updatePassword(loginId, code);
+		
+		return result;
+	}
+	
+	@GetMapping("/myDetails")
+	public String userDetails(Principal principal, Model model) {
+		UserDetailDto dto = userService.getUserInfo(principal.getName());
+		model.addAttribute("user", dto);
+		
+		return "user/details";
+	}
+	
+	@PostMapping("/modify")
+	@ResponseBody
+	public int modifyUserInfo(@RequestBody UserUpdateDto dto, Principal principal) {
+		log.info("modifyUserInfo({})", dto);
+		
+		int result = userService.updateUserInfo(dto, principal.getName());
+		log.info("result = {}", result);
+		
+		return result;
+	}
+	
+	@GetMapping("/withdrawal")
+	public String deletePage(Principal principal, Model model) {
+		log.info("deletePage()");
+		
+		UserDetailDto dto = userService.getUserInfo(principal.getName());
+		model.addAttribute("user", dto);
+		
+		return "user/delete";
+	}
+	
+	@PostMapping("/delete")
+	@ResponseBody
+	public int delete(@RequestParam("id") long id) {
+		log.info("delete({})" ,id);
+		
+		int result = userService.deleteUser(id);
+		log.info("result = {}", result);
+		
+		return result;
 	}
 	
 	@PostMapping("/logout")
