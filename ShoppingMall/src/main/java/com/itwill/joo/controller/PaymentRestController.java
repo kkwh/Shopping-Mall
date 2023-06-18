c345ec7f9619a7f485a7d9202a05ac0b14a227dc
WonjunS <seowj0710@gmail.com>
2023-06-19 00:23:25 +0900

    [서원준] Feat: 결제 정보 저장하는 controller

--- ShoppingMall/src/main/java/com/itwill/joo/controller/PaymentRestController.java
+++ ShoppingMall/src/main/java/com/itwill/joo/controller/PaymentRestController.java
@@ -1,5 +1,7 @@
 package com.itwill.joo.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +23,15 @@ public class PaymentRestController {
 	private final PaymentService paymentService;
 	
 	@PostMapping("/save")
-	public ResponseEntity<Integer> savePayment(@RequestBody PaymentInfoDto dto) {
+	public ResponseEntity<Integer> savePayment(@RequestBody Map<String, String> map) {
+		PaymentInfoDto dto = PaymentInfoDto.builder()
+				.o_id(Long.parseLong(map.get("o_id")))
+				.imp_uid(map.get("imp_uid"))
+				.merchant_uid(map.get("merchant_uid"))
+				.pay_method(map.get("pay_method"))
+				.amount(Integer.parseInt(map.get("amount")))
+				.build();
+		
 		int result = paymentService.insertPaymentData(dto);
 		
 		log.info("result = {}", result);
