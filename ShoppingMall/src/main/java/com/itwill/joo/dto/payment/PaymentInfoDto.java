9e0084f6cb1082efc1fddc9939a470a8a48d46dd
WonjunS <seowj0710@gmail.com>
2023-06-19 00:25:48 +0900

    [서원준] Feat: 결제 정보 저장할 dto 생성

--- ShoppingMall/src/main/java/com/itwill/joo/dto/payment/PaymentInfoDto.java
+++ ShoppingMall/src/main/java/com/itwill/joo/dto/payment/PaymentInfoDto.java
@@ -1,6 +1,9 @@
 package com.itwill.joo.dto.payment;
 
-import com.itwill.joo.domain.Payment;
+import java.time.LocalDateTime;
+
+import com.itwill.joo.domain.PaymentInfo;
+import com.itwill.joo.domain.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,17 +15,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class PaymentInfoDto {
-	
+
 	private long id;
 	private long o_id;
-	private String payment_status;
 	
-	public Payment toEntity() {
-		return Payment.builder()
-				.id(id)
+	private String pay_method;
+	private String imp_uid;
+	private String merchant_uid;
+	
+	private int amount;
+	
+	private LocalDateTime pcreated_time;
+	
+	public PaymentInfo toEntity() {
+		return PaymentInfo.builder()
 				.o_id(o_id)
-				.payment_status(payment_status)
+				.pay_method(pay_method)
+				.imp_uid(imp_uid)
+				.merchant_uid(merchant_uid)
+				.amount(amount)
+				.pcreated_time(pcreated_time)
 				.build();
 	}
-
+	
 }
