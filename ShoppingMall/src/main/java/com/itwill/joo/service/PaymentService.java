8b731126e21ded19a0c6ea86896bc1a1ad4e2948
WonjunS <seowj0710@gmail.com>
2023-06-19 00:26:08 +0900

    [서원준] Feat: 결제 정보 db에 저장하는 메서드 구현

--- ShoppingMall/src/main/java/com/itwill/joo/service/PaymentService.java
+++ ShoppingMall/src/main/java/com/itwill/joo/service/PaymentService.java
@@ -1,9 +1,20 @@
 package com.itwill.joo.service;
 
-import org.springframework.stereotype.Service;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itwill.joo.domain.PaymentInfo;
+import com.itwill.joo.domain.User;
 import com.itwill.joo.dto.payment.PaymentInfoDto;
+import com.itwill.joo.repository.OrderRepository;
 import com.itwill.joo.repository.PaymentRepository;
+import com.itwill.joo.repository.UserRepository;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +26,23 @@ public class PaymentService {
 	
 	private final PaymentRepository paymentRepository;
 	
+	@Transactional
+	public int verifyPayment(IamportResponse<Payment> irsp, int amount, long o_id) {		
+		PaymentInfo payment = PaymentInfo.builder()
+				.pay_method(irsp.getResponse().getPayMethod())
+				.imp_uid(irsp.getResponse().getImpUid())
+				.merchant_uid(irsp.getResponse().getMerchantUid())
+				.amount(irsp.getResponse().getAmount().intValue())
+				.o_id(o_id)
+				.build();
+		
+		return paymentRepository.insertPaymentData(payment);
+	}
+
 	public int insertPaymentData(PaymentInfoDto dto) {
-		log.info("insertPaymentData({})", dto);
+		log.info("insertPaymentDate({})", dto);
 		
 		return paymentRepository.insertPaymentData(dto.toEntity());
 	}
-
+	
 }
