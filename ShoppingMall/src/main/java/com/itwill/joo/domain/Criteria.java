26f2da380e7f12cc972b3574844919f24ffac04b
WonjunS <seowj0710@gmail.com>
2023-06-20 02:07:13 +0900

    [서원준] 도메인 클래스 추가 및 수정

--- ShoppingMall/src/main/java/com/itwill/joo/domain/Criteria.java
+++ ShoppingMall/src/main/java/com/itwill/joo/domain/Criteria.java
@@ -1,45 +1,38 @@
 package com.itwill.joo.domain;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-import com.oracle.wls.shaded.org.apache.xalan.lib.sql.QueryParameter;
-
-import lombok.Data;
-
-
-// 채한별
-// 페이징 도메인
-@Component
-@Data
+@Getter
+@Setter
+@ToString
 public class Criteria {
-    private int pageNum; // 페이지 번호
-    private int amount;    // 한 페이지의 보여줄수 있는 개수 
-    private String type;   
-    private String keyword;
-   
-    
-    // 기본 페이지를 1 페이지에 7개씩 보임
-    public Criteria() {
-        this(1, 7);
-    }
-    
-    // 매개변수로 들어오는 값을 이용하여 조정
-    public Criteria(int pageNum, int amount) {
-        this.pageNum= pageNum;
-        this.amount= amount;
-    }
-    
-    // UriComponentsBuilder를 이용해 링크 생성
-    public String getListLink() {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
-                .queryParam("pageNum", pageNum)
-                .queryParam("amount", amount);
-        
-        return builder.toUriString();
-    }
-    
-    public String[] getTypeArr() { // get으로 시작해야만 myBatis에서 찾을수있음
-        return type == null ? new String[]{} : type.split(""); 
-    }
+	
+	private int pageNum;
+	private int amount;
+	private String btype;
+	
+	private String category; // 분류
+	private String keyword;
+	
+	public Criteria() {
+		this(1, 10);
+	}
+
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+	
+	public Criteria(int pageNum, int amount, String btype) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+		this.btype = btype;
+	}
+	
+	public String[] getTypeArr() {
+		return category == null ? new String[] {} : category.split("");
+	}
+
 }
--- ShoppingMall/src/main/java/com/itwill/joo/domain/ReviewReply.java
+++ ShoppingMall/src/main/java/com/itwill/joo/domain/ReviewReply.java
@@ -16,7 +16,7 @@ public class ReviewReply {
 	private long id;
 	private long r_id; // 답변 작성한 후기 아이디
 	
-	private String content; // 후기 답변 내용
+	private String reply_content; // 후기 답변 내용
 	
 	private LocalDateTime rcreated_time; // 후기 답변 작성일자
 	private LocalDateTime rmodified_time; // 후기 답변 수정일자
