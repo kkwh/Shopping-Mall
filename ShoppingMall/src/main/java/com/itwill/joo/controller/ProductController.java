d0be776c4e4ef32b339de05f2cee021765d70135
WonjunS <seowj0710@gmail.com>
2023-06-20 02:07:42 +0900

    [서원준] controller 클래스 추가 및 수정

--- ShoppingMall/src/main/java/com/itwill/joo/controller/ProductController.java
+++ ShoppingMall/src/main/java/com/itwill/joo/controller/ProductController.java
@@ -1,15 +1,7 @@
 package com.itwill.joo.controller;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.naming.directory.SearchControls;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -22,20 +14,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.itwill.joo.controller.ProductController;
-import com.itwill.joo.domain.Criteria;
 import com.itwill.joo.domain.Product;
-import com.itwill.joo.domain.Recommend;
-import com.itwill.joo.domain.User;
+import com.itwill.joo.dto.product.ProductDto;
+import com.itwill.joo.dto.product.ProductListPageDto;
 import com.itwill.joo.dto.question.QuestionsListDto;
 import com.itwill.joo.dto.recommend.RecommendDto;
-import com.itwill.joo.dto.recommend.RecommendListDto;
 import com.itwill.joo.service.ProductService;
 import com.itwill.joo.service.QuestionService;
 import com.itwill.joo.service.RecommendService;
-import com.itwill.joo.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -53,8 +40,8 @@ public class ProductController {
     // 상품들을 리스트에 보여줌.
 	@GetMapping("/productsList")
 	public void productList(Model model) {
-	    List<Product> products = questionService.getProductList();
-	    model.addAttribute("products", products);
+		List<ProductListPageDto> p = productService.showAllProductsList();
+		model.addAttribute("products", p);
 	}
 
 	// 상품문의 정보에 product 번호를 받아옴으로써 그 제품의 상세정보를 확인
@@ -69,7 +56,9 @@ public class ProductController {
         //String loginId = principal.getName();
         //long u_id = userService.getUserInfo(loginId).getId();
        // RecommendListDto selectRecommendProductByPid = recommendService.selectRecommendProductByPid(pid);
-              
+        
+        ProductDto p = productService.userReadOneProductByid(pid);
+		model.addAttribute("product", p);
         
         // 뷰에 보여줄 데이터를 모델에 저장
         model.addAttribute("questionsList", list);
@@ -79,6 +68,11 @@ public class ProductController {
         log.info("questionsList: {}", list);
     }
     
+    /**
+     * @author 한별
+     * @param dto
+     * @return
+     */
     //제품 추천 등록
     @PostMapping
     public ResponseEntity<Integer> recommendCreate(@RequestBody RecommendDto dto) {
@@ -89,6 +83,11 @@ public class ProductController {
         return ResponseEntity.ok(result);
     }
     
+    /**
+     * @author 한별
+     * @param dto
+     * @return
+     */
     // 제품 추천 삭제
     @DeleteMapping("/{p_id}/{u_id}")
     @ResponseBody
@@ -100,14 +99,19 @@ public class ProductController {
         return ResponseEntity.ok(result);
     }
     
+    @GetMapping("/productsNewList")
+	public void productsNewList(Model model) {
+		List<ProductListPageDto> p = productService.showNewProductsLsit();
+		log.info("dto{}", p);
+		model.addAttribute("products", p);		
+	}
+	
 	@GetMapping("/productsHotList")
-	public void productsHotList() {
-
+	public void productsHotList(Model model) {
+		List<ProductListPageDto> p = productService.showHotProductsLsit();
+		log.info("dto{}", p);
+		model.addAttribute("products", p);
 	}
 
-	@GetMapping("/productsNewList")
-	public void productsNewList() {
-
-	}
 
 }
